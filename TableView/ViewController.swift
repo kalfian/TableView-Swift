59e558736ddca346ce52fead71a03424cf215d34
Kukuh <kalfian@kukuhs-macbook-pro.local>
2018-10-10 09:49:00 +0700

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Kukuh on 10/10/18.
+//  Copyright © 2018 Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
